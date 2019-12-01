71c8985c71a6bbcdbbd1cceaee42f4b218ffcd39
Makefile-dot-in <a@a.a>
2019-12-01 22:09:49 +0200

    Fixed slide level

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-pandoc -t revealjs -o index.html -s -V revealjs-url="https://revealjs.com/" presentation.md
+pandoc -t revealjs -o index.html -s -V revealjs-url="https://revealjs.com/" presentation.md --slide-level 1
